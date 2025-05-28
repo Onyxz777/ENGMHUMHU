31d7ae89949000239f43fa358b1b292bd49fa51e
Onyxz777 <chaloempongj64@gmail.com>
2025-05-28 16:03:22 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,40 +1,15 @@
-const CACHE_NAME = 'engmhumhu-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/sw.js',
-  '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("eng-cache").then(cache => {
+      return cache.addAll(["./", "./index.html"]);
     })
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
     })
-  );
-});
-
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames =>
-      Promise.all(
-        cacheNames.map(name => {
-          if (!cacheWhitelist.includes(name)) {
-            return caches.delete(name);
-          }
-        })
-      )
-    )
   );
 });
